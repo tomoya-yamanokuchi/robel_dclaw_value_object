521aa46b0beff2288a3f8567905c69694ffc14cd
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2025-04-08 12:08:00 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robel_dclaw_value_object',            # パッケージ名
+    version='0.1',                # バージョン番号
+    packages=find_packages(),     # パッケージを自動的に探してインクルード
+)
